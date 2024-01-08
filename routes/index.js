90447adf45a30e2521d0b7aa519bd0928b24d080
Rydge <96926831+rydge-rivard@users.noreply.github.com>
2024-01-08 09:11:32 -0500

    Render messages from array on index with ejs.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Rick",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Eli",
+    added: new Date(),
+  },
+];
+
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
 module.exports = router;
